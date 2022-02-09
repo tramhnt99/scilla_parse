9e363b9dab6b8cba151303e55994369ed7ab97d8
Tram Hoang <tram.hoang@u.yale-nus.edu.sg>
2022-02-09 18:25:12 +0800

    Antlr4 set up, MiniOcaml

---
+++ MiniOCamlListener.js
@@ -0,0 +1,486 @@
+// Generated from MiniOCaml.g4 by ANTLR 4.9
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete listener for a parse tree produced by MiniOCamlParser.
+export default class MiniOCamlListener extends antlr4.tree.ParseTreeListener {
+
+	// Enter a parse tree produced by MiniOCamlParser#start.
+	enterStart(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#start.
+	exitStart(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#EqualStructural.
+	enterEqualStructural(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#EqualStructural.
+	exitEqualStructural(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Multiplication.
+	enterMultiplication(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Multiplication.
+	exitMultiplication(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Or.
+	enterOr(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Or.
+	exitOr(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#LessThanOrEqual.
+	enterLessThanOrEqual(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#LessThanOrEqual.
+	exitLessThanOrEqual(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#MultiplicationFloat.
+	enterMultiplicationFloat(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#MultiplicationFloat.
+	exitMultiplicationFloat(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#String.
+	enterString(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#String.
+	exitString(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#GreaterThanOrEqual.
+	enterGreaterThanOrEqual(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#GreaterThanOrEqual.
+	exitGreaterThanOrEqual(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#LessThan.
+	enterLessThan(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#LessThan.
+	exitLessThan(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#EqualPhysical.
+	enterEqualPhysical(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#EqualPhysical.
+	exitEqualPhysical(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Number.
+	enterNumber(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Number.
+	exitNumber(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#PatternMatchingExpression.
+	enterPatternMatchingExpression(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#PatternMatchingExpression.
+	exitPatternMatchingExpression(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#GreaterThan.
+	enterGreaterThan(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#GreaterThan.
+	exitGreaterThan(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#NotEqualPhysical.
+	enterNotEqualPhysical(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#NotEqualPhysical.
+	exitNotEqualPhysical(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Char.
+	enterChar(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Char.
+	exitChar(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#DeconstructionExpression.
+	enterDeconstructionExpression(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#DeconstructionExpression.
+	exitDeconstructionExpression(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Division.
+	enterDivision(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Division.
+	exitDivision(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#ArrowFunctionExpression.
+	enterArrowFunctionExpression(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#ArrowFunctionExpression.
+	exitArrowFunctionExpression(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Boolean.
+	enterBoolean(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Boolean.
+	exitBoolean(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#LetGlobalBindingExpression.
+	enterLetGlobalBindingExpression(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#LetGlobalBindingExpression.
+	exitLetGlobalBindingExpression(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#CallExpression.
+	enterCallExpression(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#CallExpression.
+	exitCallExpression(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Parentheses.
+	enterParentheses(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Parentheses.
+	exitParentheses(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#SubtractionFloat.
+	enterSubtractionFloat(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#SubtractionFloat.
+	exitSubtractionFloat(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#PipedCallExpression.
+	enterPipedCallExpression(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#PipedCallExpression.
+	exitPipedCallExpression(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Addition.
+	enterAddition(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Addition.
+	exitAddition(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#ConditionalExpression.
+	enterConditionalExpression(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#ConditionalExpression.
+	exitConditionalExpression(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#ListDeclaration.
+	enterListDeclaration(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#ListDeclaration.
+	exitListDeclaration(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#DivisionFloat.
+	enterDivisionFloat(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#DivisionFloat.
+	exitDivisionFloat(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#FunctionDeclarationExpression.
+	enterFunctionDeclarationExpression(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#FunctionDeclarationExpression.
+	exitFunctionDeclarationExpression(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#IdentifierExpression.
+	enterIdentifierExpression(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#IdentifierExpression.
+	exitIdentifierExpression(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Float.
+	enterFloat(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Float.
+	exitFloat(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Not.
+	enterNot(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Not.
+	exitNot(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#AdditionFloat.
+	enterAdditionFloat(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#AdditionFloat.
+	exitAdditionFloat(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Subtraction.
+	enterSubtraction(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Subtraction.
+	exitSubtraction(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Concatenation.
+	enterConcatenation(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Concatenation.
+	exitConcatenation(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Modulus.
+	enterModulus(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Modulus.
+	exitModulus(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#And.
+	enterAnd(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#And.
+	exitAnd(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#LetLocalBindingExpression.
+	enterLetLocalBindingExpression(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#LetLocalBindingExpression.
+	exitLetLocalBindingExpression(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#NotEqualStructural.
+	enterNotEqualStructural(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#NotEqualStructural.
+	exitNotEqualStructural(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#Power.
+	enterPower(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#Power.
+	exitPower(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#identifier.
+	enterIdentifier(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#identifier.
+	exitIdentifier(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#arrowFunctionBody.
+	enterArrowFunctionBody(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#arrowFunctionBody.
+	exitArrowFunctionBody(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#parenthesesExpression.
+	enterParenthesesExpression(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#parenthesesExpression.
+	exitParenthesesExpression(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#arrowFunction.
+	enterArrowFunction(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#arrowFunction.
+	exitArrowFunction(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#condExp.
+	enterCondExp(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#condExp.
+	exitCondExp(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#letGlobalBinding.
+	enterLetGlobalBinding(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#letGlobalBinding.
+	exitLetGlobalBinding(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#letLocalBinding.
+	enterLetLocalBinding(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#letLocalBinding.
+	exitLetLocalBinding(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#listElement.
+	enterListElement(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#listElement.
+	exitListElement(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#listContent.
+	enterListContent(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#listContent.
+	exitListContent(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#patternMatching.
+	enterPatternMatching(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#patternMatching.
+	exitPatternMatching(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#patternBranch.
+	enterPatternBranch(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#patternBranch.
+	exitPatternBranch(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#identifierList.
+	enterIdentifierList(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#identifierList.
+	exitIdentifierList(ctx) {
+	}
+
+
+	// Enter a parse tree produced by MiniOCamlParser#functionDeclaration.
+	enterFunctionDeclaration(ctx) {
+	}
+
+	// Exit a parse tree produced by MiniOCamlParser#functionDeclaration.
+	exitFunctionDeclaration(ctx) {
+	}
+
+
+
+}
