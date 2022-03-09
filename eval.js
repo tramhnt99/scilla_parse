d0fd29c0f8b63dc78316bd0a9c5fcc9a7b7bda25
Tram Hoang <tram.hoang@u.yale-nus.edu.sg>
2022-03-09 11:42:28 +0800

    Update with environment

--- eval.js
+++ eval.js
@@ -7,24 +7,31 @@ import ST from './types.js'; //ScillaTypes
 import {parser} from './test.js';
 import { get } from 'http';
 
-// const visitor = new Visitor();
 export default class EvalVisitor {
     constructor(env) {
         this.globalEnv = env;
     }
 
-    lookup(x) {
-        return this.globalEnv[x];
+    lookup(x, env) {
+        return env[x]
+            ? env[x]
+            : console.log("Look up didn't find " + x);
     }
 
-    setEnv(k, v) {
+    setEnv(k, v, env) {
         console.log("Binding " + k + " with " + v);
-        this.globalEnv[k] = v;
-        return;
+        env[k] = v;
+        return env;
     }
 
-    getEnv() {
+    getGlobalEnv() {
         return this.globalEnv;
+    }
+
+    wrap(value, env) {
+        console.log("Wrapping value " + value);
+        console.log(env);
+        return {value: value, env: env};
     }
 
     //Returns bindings
@@ -43,72 +50,16 @@ export default class EvalVisitor {
         return undefined;
     }
 
-    visitLet(ctx) {
-        if (!ctx) {return;}
-        const x = ctx.identifier().getText();
-        const value = this.visitSimpleExp(ctx.f);
-        this.setEnv(x, value);
-        return this.visitExp(ctx.exp());
+
+    substTypeInExpr(tvar, tp, expr) {
+        
+
     }
 
-    //Returns a closure
-    visitFun(ctx) {
-        const param = ctx.identifier();
-        const clo = 
-            function(x, env) {
-                const newVisitor = new EvalVisitor(env);
-                newVisitor.setEnv(param, x);
-                return {res: newVisitor.visitExp(ctx.exp()),
-                        env: newVisitor.getEnv()};
-            };
-        return clo;
-    }
-
-    //Note, this is written in a slightly convoluted way because the keyword
-    //`this` doesn't operate properly inside `reduce` since reduce is also
-    //a closure.
-    visitApp(ctx) {
-        console.log(ctx.f_var.getText());
-        const func = this.visitSid(ctx.f_var);
-        const argsLit = ctx.args.map(arg => this.lookup(arg));
-
-        //Make a seperate copy of env, avoid issues with shadowing
-        //local env doesn't become global
-        var env = Object.assign({}, this.getEnv());
-        const fullyAppliedRes = argsLit.reduce(function(res, arg) {
-                //Apply closure to argument
-                const partialRes = res(arg, env);
-                env = partialRes.env;
-                return partialRes.res;
-            }, func);
-        return fullyAppliedRes;
-    }
-
-    visitAtomicExp(ctx) {
-        console.log("oh atomic exp!");
-        return ctx.a instanceof SP.AtomicLitContext //Literal
-            ? this.visitLiteral(ctx.a.lit())
-            : ctx.a instanceof SP.AtomicSidContext //Indentifier TODO
-            ? this.visitSid(ctx.a.sid())
-            : console.log("visitAtomicExp: Couldn't match atomic expression!");
-    }
-
-    visitMatchExp(ctx) {
-        const value = this.lookup(ctx.sid());
-        for (const clause of ctx.exp_pm_clause()) {
-            const found = this.matchClause(value, clause.pattern());
-            if (found === undefined) {} //continue
-            else {
-                //We update env for the branch
-                var env = Object.assign({}, this.getEnv());
-            }
-        }
-    }
-
-    visitSid(ctx) {
+    visitSid(ctx, env) {
         // console.log("Looking for SID " + ctx.getText());
         return ctx instanceof SP.SidNameContext
-            ? this.visitID(ctx.identifier())
+            ? this.visitID(ctx.identifier(), env)
             : ctx instanceof SP.SidSPIDContext
             ? undefined 
             : ctx instanceof SP.SidCidContext
@@ -116,16 +67,84 @@ export default class EvalVisitor {
             : console.log("visitSid: Coulnd't match sid!");
     }
 
-    visitID(ctx) {
-        console.log("Looking for ID " + ctx.getText());
+    visitID(ctx, env) {
+        // console.log("Looking for ID " + ctx.getText());
         return ctx.ID().getText()
-            ? this.lookup(ctx.ID().getText())
+            ? this.wrap(this.lookup(ctx.ID().getText(), env), env)
             : console.log("visitID: Couldn't find ID");
     }
 
-    visitLiteral(ctx) {
+    visitLet(ctx, env) {
+        if (!ctx) {return;}
+        const x = ctx.identifier().getText();
+        const value = this.visitSimpleExp(ctx.f, env);
+        const env_ = this.setEnv(x, value.value, env);
+        return this.visitExp(ctx.exp(), env_);
+    }
+
+    //Returns a closure
+    visitFun(ctx, env) {
+        const param = ctx.identifier();
+        const clo = 
+            function(x) {
+                const newVisitor = new EvalVisitor(env);
+                const env_ = newVisitor.setEnv(param, x, env);
+                return newVisitor.visitExp(ctx.exp(), env_);
+            };
+        return this.wrap(clo, env);
+    }
+
+    //Note, this is written in a slightly convoluted way because the keyword
+    //`this` doesn't operate properly inside `reduce` since reduce is also
+    //a closure.
+    visitApp(ctx, env) {
+        console.log("Env at visitApp ");
+        console.log(env);
+        const func = this.visitSid(ctx.f_var, env);
+        const argsLit = ctx.args.map(arg => this.lookup(arg.getText(), env));
+
+        //Make a seperate copy of env, avoid issues with shadowing
+        //local env doesn't become global
+        const fullyAppliedRes = argsLit.reduce(function(res, arg) {
+                //Apply closure to arg
+                const partialRes = res.value(arg);
+                return partialRes;
+            }, func);
+        return fullyAppliedRes;
+    }
+
+    visitAtomicExp(ctx, env) {
+        console.log("oh atomic exp!");
+        return ctx.a instanceof SP.AtomicLitContext //Literal
+            ? this.visitLiteral(ctx.a.lit(), env)
+            : ctx.a instanceof SP.AtomicSidContext //Indentifier TODO
+            ? this.visitSid(ctx.a.sid(), env)
+            : console.log("visitAtomicExp: Couldn't match atomic expression!");
+    }
+
+    visitMatchExp(ctx, env) {
+        const value = this.lookup(ctx.sid());
+        for (const clause of ctx.exp_pm_clause()) {
+            const binds = this.matchClause(value, clause.pattern());
+            if (binds === undefined) {} //continue
+            else {
+                //We update env for the branch
+                let env_ = env;
+                for (let k in binds) {
+                    env_ = this.setEnv(k, binds[k], env_);
+                }
+                this.visitExp(clause.exp(), env_);
+            }
+        }
+    }
+
+    visitTFun(ctx, env) {
+
+    }
+
+    visitLiteral(ctx, env) {
         // console.log("oh literal! " + ctx.getText());
-        return ctx instanceof SP.LitCidContext
+        const val =  ctx instanceof SP.LitCidContext
             ? undefined //cid
             : ctx instanceof SP.LitIntContext
             ? parseInt(ctx.int_().getText()) //integer
@@ -142,35 +161,40 @@ export default class EvalVisitor {
             : ctx instanceof SP.LitBoolContext
             ? (ctx.b.getText() === "True")
             : console.log("visitLiteral: Couldn't match literal");
+        return this.wrap(val, env)
     }
     
-    visitSimpleExp(ctx) {
+    visitSimpleExp(ctx, env) {
         if (!ctx) {return;}
         return ctx instanceof SP.LetContext
-            ? this.visitLet(ctx)
+            ? this.visitLet(ctx, env)
             : ctx instanceof SP.AtomicContext
-            ? this.visitAtomicExp(ctx)
+            ? this.visitAtomicExp(ctx, env)
             : ctx instanceof SP.FunContext
-            ? this.visitFun(ctx)
+            ? this.visitFun(ctx, env)
             : ctx instanceof SP.AppContext
-            ? this.visitApp(ctx)
+            ? this.visitApp(ctx, env)
             : ctx instanceof SP.MessageContext
             ? console.log("TODO: Resolve Message")
             : ctx instanceof SP.BuiltinContext
             ? console.log("TODO: Builtin") //All builtins will be saved as closures
+            : ctx instanceof SP.ConstructorContext
+            ? console.log("TODO: DataConstructorApp")
             : ctx instanceof SP.MatchContext
-            ? this.visitMatchExp(ctx)
+            ? this.visitMatchExp(ctx, env)
+            : ctx instanceof SP.TFunContext
+            ? this.visitTFun(ctx, env)
             : undefined;
     }
 
-    visitExp(ctx) {
-        return (this.visitSimpleExp(ctx.simple_exp()));
+    visitExp(ctx, env) {
+        return (this.visitSimpleExp(ctx.simple_exp(), env));
     }
 
     visitChildren(ctx) {
         if (!ctx) {return;}
         return ctx instanceof SP.Simple_expContext
-            ? console.log(this.visitSimpleExp(ctx))
+            ? console.log(this.visitSimpleExp(ctx, {}))
             : undefined;
     }
 
